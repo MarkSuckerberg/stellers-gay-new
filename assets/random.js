fcd8d503cdf832d0ee4f72d5e4305c17543aa004
Mark Suckerberg <mark@suckerberg.gay>
2024-07-05 11:35:23 -0500

    hopefully fixes it for good this time

--- assets/random.js
+++ assets/random.js
@@ -6,7 +6,11 @@ const quotes = fetch("assets/quotes.json", {
 	mode: "no-cors",
 })
 	.then((response) => response.json())
-	.then((data) => data.quotes);
-
-document.getElementById("footer").textContent = quotes[Math.floor(random * quotes.length)];
+	.then((data) => data.quotes)
+	.then((quotes) => {
+		document.getElementById("footer").textContent = quotes[Math.floor(random * quotes.length)];
+	}).catch((error) => {
+		console.error("Failed to fetch quotes", error);
+		document.getElementById("footer").textContent = "Tell Mark he broke the quote grabber again!";
+	})
 
