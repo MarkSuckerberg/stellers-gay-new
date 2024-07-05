abc19c6f156f644a9b5c26073fef88b2f77c9d3b
Mark Suckerberg <mark@suckerberg.gay>
2024-07-05 03:11:43 -0500

    changes quotes script

--- assets/random.js
+++ assets/random.js
@@ -1,29 +1,8 @@
 const random = Math.random();
 
-const messages = [
-	"The keyboard shortcuts do actually work! Just not well.",
-	"Hello world!",
-	"Did you know that Steller's Jays and Blue Jays can hybridize?",
-	"CAW!",
-	"Something done poorly on purpose is still bad.",
-	"If you do small things, even though big things exist: you're on your way to crushing it! -Tom Cardy",
-	"Hello!",
-	"Don't go gentle into that good night. Rage, rage against the dying of the light.",
-	"The Marquis of Carabass?",
-	"I originally made this page with html tables.",
-	"Try the dip!",
-	"Now where could my pipe be?",
-	"Error 404: Witty quote not found.",
-	"Coffee...",
-	"The Tufted Titmice are coming.",
-	"Caw?",
-	"Birds.",
-	"Isn't Chicago in Denver?",
-	"Is it Suckerberg or Suckerbird?",
-	"Have you looked at a bird today?",
-	"I joined the war on frames on the side of AJAX.",
-	"I looked up how to center an element with CSS at least 7 times in the making of this site.",
-];
+const quotes = fetch("/assets/quotes.json")
+	.then((response) => response.json())
+	.then((data) => data.quotes);
 
-document.getElementById("footer").textContent = messages[Math.floor(random * messages.length)];
+document.getElementById("footer").textContent = quotes[Math.floor(random * quotes.length)];
 
